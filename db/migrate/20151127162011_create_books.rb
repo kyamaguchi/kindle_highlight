34e327925bef9466d42f58e81fe5878c3fbbf0b2
Kazuho Yamaguchi <kzh.yap@gmail.com>
2015-11-28 01:20:37 +0900

    Add book model
    $ rails g model book asin title author

---
+++ db/migrate/20151127162011_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+      t.string :asin
+      t.string :title
+      t.string :author
+
+      t.timestamps null: false
+    end
+  end
+end
