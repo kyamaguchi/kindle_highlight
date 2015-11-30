d7b7a1164bc26ba6965276d4b26a9cff8455edf8
Kazuho Yamaguchi <kzh.yap@gmail.com>
2015-12-01 00:21:05 +0900

    Increase default per page

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -13,7 +13,7 @@ class Admin::ApplicationController < Administrate::ApplicationController
 
   # Override this value to specify the number of elements to display at a time
   # on index pages. Defaults to 20.
-  # def records_per_page
-  #   params[:per_page] || 20
-  # end
+  def records_per_page
+    params[:per_page] || 50
+  end
 end
