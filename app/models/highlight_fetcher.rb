7c539d2bbc43e5453896701f670553ac436850f7
Kazuho Yamaguchi <kzh.yap@gmail.com>
2015-12-01 10:06:13 +0900

    Refactor highlight fetching using model

---
+++ app/models/highlight_fetcher.rb
@@ -0,0 +1,26 @@
+class HighlightFetcher
+
+  def self.run
+    raise "Please define login in .env OR run with $ rake kindle:fetch_highlights AMAZON_USERNAME=your_username@example.com AMAZON_PASSWORD=xxx" if ENV['AMAZON_USERNAME'].blank? || ENV['AMAZON_PASSWORD'].blank?
+    k = Kindle::Highlights.new(login: ENV['AMAZON_USERNAME'], password: ENV['AMAZON_PASSWORD'])
+    k.fetch_highlights.group_by(&:asin).each do |asin, highlights|
+      title = highlights.first.title
+      Rails.logger.info "Saving #{title} http://www.amazon.co.jp/gp/product/#{asin}/"
+      book = Book.where(asin: asin).first_or_create! do |b|
+        Rails.logger.info "Creating #{title}"
+        b.title = title
+        b.author = highlights.first.author
+      end
+      book.update_attribute(:last_annotated_on, highlights.first.last_annotated_on)
+      highlights.each do |h|
+        book.highlights.where(content: h.highlight).first_or_create! do |obj|
+          obj.location = h.location
+          obj.annotation_id = h.id
+          obj.note = h.note
+          obj.note_id = h.note_id
+          obj.fetched_at = Time.now
+        end
+      end
+    end
+  end
+end
