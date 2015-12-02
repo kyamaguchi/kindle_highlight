fa9abba69df3794b95159d73c7b99ce61b391e97
Kazuho Yamaguchi <kzh.yap@gmail.com>
2015-12-02 16:47:34 +0900

    Fix subject of summary mail

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -3,7 +3,7 @@ class Notifier < ApplicationMailer
     @books = books
     mail_to = ENV['MAIL_TO'].presence || ENV['AMAZON_USERNAME']
     if mail_to.present?
-      mail(to: mail_to)
+      mail(to: mail_to, subject: "[Kindle] #{books.recent.map{|b| b.title.truncate(10, omission: '') }.join(',')}")
     else
       Rails.logger.error("ENV['MAIL_TO'] or ENV['AMAZON_USERNAME'] is not defined.")
     end
