2fe668579edd4789abf8c4b3ae46eb7d3c47203d
Kazuho Yamaguchi <kzh.yap@gmail.com>
2015-12-04 23:30:57 +0900

    Add link to admin top page to emails

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -8,6 +8,9 @@ if Rails.env.production?
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
+  host = "#{ENV['HEROKU_APP_NAME'] || 'myapp'}.herokuapp.com"
+  ActionMailer::Base.default_url_options = { host:host }
 elsif Rails.env.development?
   ActionMailer::Base.delivery_method = :letter_opener
+  ActionMailer::Base.default_url_options = { host:'localhost:3000' }
 end
