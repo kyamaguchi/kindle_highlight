73e2b0db730b1ad7aa59a5fc122378dc54b27a0a
Kazuho Yamaguchi <kzh.yap@gmail.com>
2016-05-25 13:48:14 +0900

    Add spec for highlights

---
+++ spec/factories/highlights.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :highlight do
+    book
+    content "アイウエオ"
+    location 677
+    annotation_id "ABC123456"
+    note ""
+    note_id ""
+    fetched_at 22.hours.ago
+  end
+
+end
--- spec/features/basic_spec.rb
+++ spec/features/basic_spec.rb
@@ -4,14 +4,20 @@ RSpec.describe "Basic", type: :feature do
 
   before do
     @book = create(:book)
+    create(:highlight, content: "ABC", book: @book)
+    create(:highlight, content: "DEF", book: @book)
   end
 
   it "move around pages" do
     visit '/admin'
     within('#page-title') { expect(page).to have_content('Books') }
-    within('.collection-data tbody tr') { expect(page).to have_content(@book.title) }
+    within('.collection-data tbody tr') do
+      expect(page).to have_content(@book.title)
+      expect(page).to have_content("2 highlights")
+    end
 
     first('.collection-data tbody tr').click
     within('header.header') { expect(page).to have_content('Book') }
+    expect(first('#highlight_summary').value).to include("ABC", "DEF")
   end
 end
