aef4010f8c20044bdb1b3738d75215492591606d
Kazuho Yamaguchi <kzh.yap@gmail.com>
2017-06-14 16:08:14 +0900

    Use GOOGLE_CHROME_BIN instead of GOOGLE_CHROME_SHIM
    The last error was:
    Selenium::WebDriver::Error::UnknownError: unknown error: no chrome binary at /tmp/build_cbbcfd57d7ec95a0cd8592b05a52dd69/.apt/usr/bin/google-chrome-stable
      (Driver info: chromedriver=2.30.477691 (6ee44a7247c639c0703f291d320bdf05c1531b57),platform=Linux 3.13.0-112-generic x86_64)

--- config/initializers/heroku.rb
+++ config/initializers/heroku.rb
@@ -1,6 +1,7 @@
 # https://github.com/heroku/heroku-buildpack-google-chrome
-if ENV['GOOGLE_CHROME_SHIM'].present?
-  chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+if ENV['GOOGLE_CHROME_BIN'].present?
+  require "selenium-webdriver"
+  chrome_bin = ENV.fetch('GOOGLE_CHROME_BIN', nil)
   chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
   Capybara.register_driver :chrome do |app|
     Capybara::Selenium::Driver.new(
